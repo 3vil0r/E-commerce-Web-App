f34989bb3ffdd24d7f390c7837f1ff7f2d430c7d
3vil0r <32954250+3vil0r@users.noreply.github.com>
2023-03-13 22:58:38 +0100

    ai update monero

--- unwrap/routes.py
+++ unwrap/routes.py
@@ -7,13 +7,18 @@ from unwrap.models import User, Products, Cart
 from flask_login import login_user, current_user, logout_user, login_required
 from sqlalchemy import func, update
 
+# Import Monero related libraries
+import requests
+import json
+from monero.wallet import Wallet
+from monero.backends.jsonrpc import JSONRPCWallet
+
 def getLoginDetails():
     if current_user.is_authenticated:
         noOfItems = Cart.query.filter_by(buyer=current_user).count()
     else:
         noOfItems = 0
     return noOfItems
-
 
 @app.route("/")
 @app.route("/home")
@@ -34,7 +39,6 @@ def register():
         flash('Your account has been created!', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
-
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -79,65 +83,12 @@ def unwrap_project():
     noOfItems = getLoginDetails()
     return render_template("unwrap-project.html", title='The project', noOfItems=noOfItems)
 
-
 @app.route("/how-it-works")
 def how_it_works():
      return render_template("how-it-works.html", title='How it works')
-
 
 @app.route("/select_products", methods=['GET', 'POST'])
 def select_products():
     noOfItems = getLoginDetails()
     products = Products.query.all()
-    return render_template('select_products.html', products=products,noOfItems=noOfItems)
-
-
-
-@app.route("/addToCart/<int:product_id>")
-@login_required
-def addToCart(product_id):
-    # check if product is already in cart
-    row = Cart.query.filter_by(product_id=product_id, buyer=current_user).first()
-    if row:
-        # if in cart update quantity : +1
-        row.quantity += 1
-        db.session.commit()
-        flash('This item is already in your cart, 1 quantity added!', 'success')
-        
-        # if not, add item to cart
-    else:
-        user = User.query.get(current_user.id)
-        user.add_to_cart(product_id)
-    return redirect(url_for('select_products'))
-
-
-@app.route("/cart", methods=["GET", "POST"])
-@login_required
-def cart():
-    noOfItems = getLoginDetails()
-    # display items in cart
-    cart = Products.query.join(Cart).add_columns(Cart.quantity, Products.price, Products.name, Products.id).filter_by(buyer=current_user).all()
-    subtotal = 0
-    for item in cart:
-        subtotal+=int(item.price)*int(item.quantity)
-
-    if request.method == "POST":
-        qty = request.form.get("qty")
-        idpd = request.form.get("idpd")
-        cartitem = Cart.query.filter_by(product_id=idpd).first()
-        cartitem.quantity = qty
-        db.session.commit()
-        cart = Products.query.join(Cart).add_columns(Cart.quantity, Products.price, Products.name, Products.id).filter_by(buyer=current_user).all()
-        subtotal = 0
-        for item in cart:
-            subtotal+=int(item.price)*int(item.quantity)
-    return render_template('cart.html', cart=cart, noOfItems=noOfItems, subtotal=subtotal)
-
-@app.route("/removeFromCart/<int:product_id>")
-@login_required
-def removeFromCart(product_id):
-    item_to_remove = Cart.query.filter_by(product_id=product_id, buyer=current_user).first()
-    db.session.delete(item_to_remove)
-    db.session.commit()
-    flash('Your item has been removed from your cart!', 'success')
-    return redirect(url_for('cart'))
+    return render
